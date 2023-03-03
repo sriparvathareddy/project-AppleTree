1fc309980fe7d8b12edd93ca4e7e08e7d3ba8232
Samuel Golovin <samuel.samyco@gmail.com>
2023-03-03 17:35:36 -0600

    set up basic apple tree

--- Apple_Tree.py
+++ Apple_Tree.py
@@ -1,1 +1,20 @@
-dgkxjegclkefckjefb ,mje
+from array_queue import ArrayQueue
+from array_stack import ArrayStack
+
+class AppleTree:
+    def __init__(self):
+        self.appleTree = ArrayQueue()
+        for i in range(100):
+            self.appleTree.enqueue("apple")
+
+    def applesLeft(self):
+        print("There are " + str(len(self.appleTree)) + " left in the tree")
+
+    def takeApple(self):
+        self.appleTree.dequeue()
+        
+appleTree1 = AppleTree()
+appleTree1.takeApple()
+appleTree1.applesLeft()
+    
+#(apple tree, basket, worker, wagon, storage).
